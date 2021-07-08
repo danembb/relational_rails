2492f17101c55b448b92d456d9ad8770fd68d2e0
danembb <dbrophy720@gmail.com>
2021-07-08 08:12:30 -0600

    Refactor test file for cat_cafe and update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/cat-cafes', to: 'cat_cafe#index'
+  get '/cat_cafes', to: 'cat_cafe#index'
 end
--- spec/features/cat_cafes/cat_cafe_spec.rb
+++ spec/features/cat_cafes/cat_cafe_spec.rb
@@ -6,10 +6,15 @@ RSpec.describe 'cat cafe page', type: :feature do
   #When I visit '/parents'
   #Then I see the name of each parent record in the system
   it 'can see the name cat cafe when visiting /parents' do
-    cat_cafe = CatCafe.create!(name: 'Denver Cat Caves', capacity: '21') #assuming default values will generate and be true?
+    cat_cafe1 = CatCafe.create!(name: 'Denver Cat Caves', capacity: '22') #assuming default values will generate and be true?
+    cat_cafe2 = CatCafe.create!(name: 'The Meowning Sip', capacity: '15')
 
-    expect(page).to have_content(cat_cafe.name)
-    expect(page).to have_content(cat_cafe.serves_food)
+    visit "/cat_cafes"
+
+    expect(page).to have_content(cat_cafe1.name)
+    expect(page).to have_content(cat_cafe2.name)
+    expect(page).to have_content(cat_cafe1.serves_food)
+    expect(page).to have_content(cat_cafe2.serves_food)
 
   end
 end
