1b0370408cb37627f75ebb1f4e9e8377b08e4958
danembb <dbrophy720@gmail.com>
2021-07-08 11:51:23 -0600

    Refactor test files for passing tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/cat_cafes', to: 'cat_cafe#index'
-  
-  get '/dog-parks', to: 'dog_parks#index'
-  get 'dog-parks/:id', to:'dog_parks#show'
+  get '/cat_cafes', to: 'cat_cafes#index'
+
+  get '/dog_parks', to: 'dog_parks#index'
+  get 'dog_parks/:id', to: 'dog_parks#show'
 end
