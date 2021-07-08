717e282ab000b183cb83462140fc4ee3d30e086d
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-08 16:07:37 -0600

    Build test for dogs show page

--- spec/features/dogs/show_spec.rb
+++ spec/features/dogs/show_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'the dog show page' do
                             off_leash: false,
                             food: true)
     @dog_1 = Dog.create!(name: 'Alfie',
-                            plays_fetch: true,
+                            plays_fetch: false,
                             age: 1.5,
                             breed: 'Australian Shepherd',
                             dog_park_id: @park_1.id)
@@ -18,5 +18,49 @@ RSpec.describe 'the dog show page' do
                             dog_park_id: @park_1.id)
   end
 
-  describe
+  it 'displays dog name' do
+    visit "/dogs/#{@dog_1.id}"
+
+    expect(page).to have_content(@dog_1.name)
+    expect(page).to_not have_content(@dog_2.name)
+  end
+
+  it 'displays dog park id' do
+    visit "/dogs/#{@dog_1.id}"
+
+    expect(page).to have_content("Dog park id: #{@dog_1.dog_park_id}")
+  end
+
+  it 'displays if dog plays fetch' do
+    visit "/dogs/#{@dog_1.id}"
+
+    expect(page).to have_content("Plays fetch: #{@dog_1.plays_fetch}")
+    expect(page).to_not have_content("Plays fetch: #{@dog_2.plays_fetch}")
+  end
+
+  it 'displays dog age' do
+    visit "/dogs/#{@dog_1.id}"
+
+    expect(page).to have_content("Age: #{@dog_1.age}")
+    expect(page).to_not have_content("Age: #{@dog_2.age}")
+  end
+
+  it 'displays dog breed' do
+    visit "/dogs/#{@dog_1.id}"
+
+    expect(page).to have_content("Breed: #{@dog_1.breed}")
+    expect(page).to_not have_content("Breed: #{@dog_2.breed}")
+  end
+
+  it 'displays dog created at' do
+    visit "/dogs/#{@dog_1.id}"
+
+    expect(page).to have_content("Created at: #{@dog_1.created_at}")
+  end
+
+  it 'displays dog updated at' do
+    visit "/dogs/#{@dog_1.id}"
+
+    expect(page).to have_content("Updated at: #{@dog_1.updated_at}")
+  end
 end
