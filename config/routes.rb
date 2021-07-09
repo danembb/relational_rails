eb7444f39db16d17bcb2e3fcc900e4a98c88ba2a
danembb <dbrophy720@gmail.com>
2021-07-09 09:02:55 -0600

    Create routes and controller files for cats child class

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/cat_cafes', to: 'cat_cafes#index'
   get '/cat_cafes/:id', to: 'cat_cafes#show'
 
+  get '/cats', to: 'cats#index'
+
   get '/dog_parks', to: 'dog_parks#index'
   get 'dog_parks/:id', to: 'dog_parks#show'
 end
