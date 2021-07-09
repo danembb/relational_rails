2f24f89e949ecfe6e89551b9ff170e658cc7a433
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-09 17:16:44 -0600

    Add test for dog count

--- spec/features/dog_parks/show_spec.rb
+++ spec/features/dog_parks/show_spec.rb
@@ -10,6 +10,21 @@ RSpec.describe 'the dog park show page' do
                             capacity: 50,
                             off_leash: true,
                             food: false)
+    @dog_1 = Dog.create!(name: 'Alfie',
+                            plays_fetch: true,
+                            age: 1.5,
+                            breed: 'Australian Shepherd',
+                            dog_park_id: @park_1.id)
+    @dog_2 = Dog.create!(name: 'Hazel',
+                            plays_fetch: true,
+                            age: 2,
+                            breed: 'Nova Scotia Duck Tolling Retriever',
+                            dog_park_id: @park_1.id)
+    @dog_3 = Dog.create!(name: 'Scout',
+                            plays_fetch: false,
+                            age: 5,
+                            breed: 'Labrador Retriever',
+                            dog_park_id: @park_2.id)
   end
 
   it 'displays the dog park name' do
@@ -38,5 +53,23 @@ RSpec.describe 'the dog park show page' do
 
     expect(page).to have_content("Food for sale: #{@park_1.food}")
     expect(page).to_not have_content("Food for sale: #{@park_2.food}")
+  end
+
+  it 'displays dog park created at' do
+    visit "/dog_parks/#{@park_1.id}"
+
+    expect(page).to have_content("Created at: #{@park_1.created_at}")
+  end
+
+  it 'displays dog park updated at' do
+    visit "/dog_parks/#{@park_1.id}"
+
+    expect(page).to have_content("Created at: #{@park_1.updated_at}")
+  end
+
+  it 'displays count dogs in the dog park' do
+    visit "/dog_parks/#{@park_1.id}"
+
+    expect(page).to have_content("Dog count: #{@park_1.dog_count}")
   end
 end
