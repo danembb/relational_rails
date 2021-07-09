78eee5223cf9534e1daaaa9a3afe9ecc23a0af77
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-09 16:46:51 -0600

    Comment out before each

--- spec/models/dog_park_spec.rb
+++ spec/models/dog_park_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe DogPark do
   it {should have_many :dogs}
 
-  describe 'instance methods' do
-    before(:each) do
-      @park_1 = DogPark.create!(name: "Woofs and Wags",
-                              capacity: 25,
-                              off_leash: false,
-                              food: true)
-      @park_2 = DogPark.create!(name: "Puppy Prairie",
-                              capacity: 50,
-                              off_leash: true,
-                              food: false)
-    end
-  end
+  # describe 'instance methods' do
+  #   before(:each) do
+  #     @park_1 = DogPark.create!(name: "Woofs and Wags",
+  #                             capacity: 25,
+  #                             off_leash: false,
+  #                             food: true)
+  #     @park_2 = DogPark.create!(name: "Puppy Prairie",
+  #                             capacity: 50,
+  #                             off_leash: true,
+  #                             food: false)
+  #   end
+  # end
 end
