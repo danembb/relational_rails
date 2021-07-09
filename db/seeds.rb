c9709f633d93300afd847930f9c8d41635d45ca0
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-09 15:12:46 -0600

    Add initial data for dog and dog park

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@park_1 = DogPark.create!(name: "Woofs and Wags",
+                        capacity: 25,
+                        off_leash: false,
+                        food: true)
+@park_2 = DogPark.create!(name: "Puppy Prairie",
+                        capacity: 50,
+                        off_leash: true,
+                        food: false)
+@dog_1 = Dog.create!(name: 'Alfie',
+                        plays_fetch: false,
+                        age: 1.5,
+                        breed: 'Australian Shepherd',
+                        dog_park_id: @park_1.id)
+@dog_2 = Dog.create!(name: 'Hazel',
+                        plays_fetch: true,
+                        age: 2,
+                        breed: 'Nova Scotia Duck Tolling Retriever',
+                        dog_park_id: @park_1.id)
+@dog_3 = Dog.create!(name: 'Scout',
+                        plays_fetch: false,
+                        age: 5,
+                        breed: 'Labrador Retriever',
+                        dog_park_id: @park_2.id)
