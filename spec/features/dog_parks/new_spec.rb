d1a4d4708a295a1be26be0aca6e3119777ac24e4
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-10 14:38:32 -0600

    Build test for build dog park form

---
+++ spec/features/dog_parks/new_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'the Dog Park creation' do
+  it 'can create a new dog park' do
+    visit '/dog_parks/new'
+
+    fill_in('Name', with: 'Doggy Territory')
+    fill_in('Capacity', with: 20)
+    fill_in('off_leash', with: false)
+    fill_in('food', with: false)
+    click_button('Create Dog Park')
+
+    new_dog_park = DogPark.last
+
+    expect(current_path).to eq('/dog_parks')
+    expect(new_dog_park.name).to eq('Doggy Territory')
+    expect(page).to have_content('Doggy Territory')
+    expect(page).to have_content(new_dog_park.capacity)
+  end
+end
