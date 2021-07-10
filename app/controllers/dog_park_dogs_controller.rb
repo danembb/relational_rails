745e898252606f2f29da2e023210a6bd81681374
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-10 15:31:36 -0600

    build new create and dog_params methods

--- app/controllers/dog_park_dogs_controller.rb
+++ app/controllers/dog_park_dogs_controller.rb
@@ -3,4 +3,17 @@ class DogParkDogsController < ApplicationController
     @dog_park = DogPark.find(params[:dog_park_id])
     @dogs = @dog_park.dogs
   end
+
+  def new
+    @dog_park = DogPark.find(params[:dog_park_id])
+  end
+
+  def create
+    dog = Dog.create(dog_params)
+    redirect_to "/dog_parks/#{dog.dog_park_id}/dogs"
+  end
+
+  def dog_params
+    params.permit(:name, :plays_fetch, :age, :breed, :dog_park_id)
+  end
 end
