8390f789e0e2cb06bb97f462f6823a399b3c8ca3
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-10 15:32:13 -0600

    Add get and post routes for create dog

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   get '/dog_parks/new', to: 'dog_parks#new'
   post '/dog_parks', to: 'dog_parks#create'
   get '/dog_parks/:id', to: 'dog_parks#show'
+
   get '/dog_parks/:dog_park_id/dogs', to: 'dog_park_dogs#index'
+  get '/dog_parks/:dog_park_id/dogs/new', to: 'dog_park_dogs#new'
+  post '/dog_parks/:dog_park_id/dogs/', to: 'dog_park_dogs#create'
 
   get '/dogs', to: 'dogs#index'
   get '/dogs/:id', to: 'dogs#show'
