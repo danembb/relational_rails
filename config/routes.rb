1a95835e105300aa7325c87987c9997cd745ecfd
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-10 14:37:37 -0600

    Add get for new dog park and post for create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/cat_cafes', to: 'cat_cafes#index'
 
   get '/dog_parks', to: 'dog_parks#index'
+  get '/dog_parks/new', to: 'dog_parks#new'
+  post '/dog_parks', to: 'dog_parks#create'
   get '/dog_parks/:id', to: 'dog_parks#show'
   get '/dog_parks/:dog_park_id/dogs', to: 'dog_park_dogs#index'
 
