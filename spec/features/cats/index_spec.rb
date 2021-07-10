398c8562031bf91b0f3cdf323379eec73d6573f8
danembb <dbrophy720@gmail.com>
2021-07-09 21:11:04 -0600

    Create cat index page test file and ensure passing tests

---
+++ spec/features/cats/index_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe 'cat index page' do
+  # As a visitor
+  # When I visit '/child_table_name'
+  # Then I see each Child in the system including the Child's attributes:
+  before(:each) do
+    @cat_cafe1 = CatCafe.create!(name: "Denver Cat Caves",
+                                 capacity: "22")
+    @cat_1 = Cat.create!(name: 'Bb',
+                         friendly: true,
+                         age: 5,
+                         color: 'White and black',
+                         cat_cafe_id: @cat_cafe1.id)
+    @cat_2 = Cat.create!(name: 'Nico',
+                         friendly: true,
+                         age: 6,
+                         color: 'Black and white',
+                         cat_cafe_id: @cat_cafe1.id)
+  end
+
+  it 'can see cat cafe' do
+    visit '/cats'
+    expect(page).to have_content("Cat cafe id: #{@cat_cafe1.id}")
+  end
+
+  it 'can see cat names' do
+    visit '/cats'
+    expect(page).to have_content(@cat_1.name)
+    expect(page).to have_content(@cat_2.name)
+  end
+
+  it 'can see if cats are friendly' do
+    visit '/cats'
+    expect(page).to have_content(@cat_1.friendly)
+    expect(page).to have_content(@cat_2.friendly)
+  end
+
+  it 'can see all cats ages' do
+    visit '/cats'
+    expect(page).to have_content(@cat_1.age)
+    expect(page).to have_content(@cat_2.age)
+  end
+
+  it 'can see all cats color' do
+    visit '/cats'
+    expect(page).to have_content(@cat_1.color)
+    expect(page).to have_content(@cat_2.color)
+  end
+end
--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,15 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe 'Cat index page' do
+RSpec.describe Cat do
   describe 'relationships' do
     it { should belong_to(:cat_cafe) }
-  end
-
-  before(:each) do
-    @cat_1 = Dog.create!(name: 'Bb',
-                         friendly: true,
-                         age: 5,
-                         color: 'White and black')
   end
 
 
