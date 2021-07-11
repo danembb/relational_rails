78644bb8e9de060c0ab92ff2fa2be5a76e01ae01
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-11 14:47:29 -0600

    Build edit update and dog params methods

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -6,4 +6,18 @@ class DogsController < ApplicationController
   def show
     @dog = Dog.find(params[:id])
   end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    dog = Dog.find(params[:id])
+    dog.update(dog_params)
+    redirect_to "/dogs/#{dog.id}"
+  end
+
+  def dog_params
+    params.permit(:name, :plays_fetch, :age, :breed, :dog_park_id)
+  end
 end
