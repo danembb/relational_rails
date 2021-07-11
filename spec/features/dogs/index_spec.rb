3fec952528e57474cc376c8d49b45b4d419cbd4d
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-11 15:19:51 -0600

    Add test for if index only displays dogs that play fetch

--- spec/features/dogs/index_spec.rb
+++ spec/features/dogs/index_spec.rb
@@ -18,68 +18,96 @@ RSpec.describe 'Dog index page' do
                             dog_park_id: @park_1.id)
   end
 
-  it 'can see all dog names' do
-    visit '/dogs'
+  describe 'displays dogs and their attributes' do
+    it 'can see all dog names' do
+      visit '/dogs'
 
-    expect(page).to have_content(@dog_1.name)
-    expect(page).to have_content(@dog_2.name)
+      expect(page).to have_content(@dog_1.name)
+      expect(page).to have_content(@dog_2.name)
+    end
+
+    it 'can see all dog parks' do
+      visit '/dogs'
+
+      expect(page).to have_content("Dog park id: #{@dog_1.dog_park_id}")
+      expect(page).to have_content("Dog park id: #{@dog_2.dog_park_id}")
+    end
+
+    it 'can see if each dog plays fetch' do
+      visit '/dogs'
+
+      expect(page).to have_content("Plays fetch: #{@dog_1.plays_fetch}")
+      expect(page).to have_content("Plays fetch: #{@dog_2.plays_fetch}")
+    end
+
+    it 'can see all dog ages' do
+      visit '/dogs'
+
+      expect(page).to have_content("Age: #{@dog_1.age}")
+      expect(page).to have_content("Age: #{@dog_2.age}")
+    end
+
+    it 'can see all dog breeds' do
+      visit '/dogs'
+
+      expect(page).to have_content("Breed: #{@dog_1.breed}")
+      expect(page).to have_content("Breed: #{@dog_2.breed}")
+    end
+
+    it 'can see all dog created at timestamps' do
+      visit '/dogs'
+
+      expect(page).to have_content("Created at: #{@dog_1.created_at}")
+      expect(page).to have_content("Created at: #{@dog_2.created_at}")
+    end
+
+    it 'can see all dog updated at timestamps' do
+      visit '/dogs'
+
+      expect(page).to have_content("Updated at: #{@dog_1.updated_at}")
+      expect(page).to have_content("Updated at: #{@dog_2.updated_at}")
+    end
   end
 
-  it 'can see all dog parks' do
-    visit '/dogs'
+  describe 'hyperlinks' do
+    it 'links to dog park index' do
+      visit '/dogs'
 
-    expect(page).to have_content("Dog park id: #{@dog_1.dog_park_id}")
-    expect(page).to have_content("Dog park id: #{@dog_2.dog_park_id}")
+      click_on 'Dog Parks Index'
+
+      expect(current_path).to eq('/dog_parks')
+    end
+
+    it 'links to dogs index' do
+      visit '/dogs'
+
+      click_on 'Dogs Index'
+
+      expect(current_path).to eq('/dogs')
+    end
   end
 
-  it 'can see if each dog plays fetch' do
-    visit '/dogs'
+  describe 'content filtering' do
+  it 'does not show dogs that do not play fetch' do
+      dog_3 = Dog.create!(name: 'Scout',
+                              plays_fetch: false,
+                              age: 5,
+                              breed: 'Labrador Retriever',
+                              dog_park_id: @park_1.id)
 
-    expect(page).to have_content("Plays fetch: #{@dog_1.plays_fetch}")
-    expect(page).to have_content("Plays fetch: #{@dog_2.plays_fetch}")
-  end
+      visit '/dogs'
 
-  it 'can see all dog ages' do
-    visit '/dogs'
+      expect(page).to_not have_content(dog_3.name)
+      expect(page).to_not have_content("Age: #{dog_3.age}")
+      expect(page).to_not have_content("Breed: #{dog_3.breed}")
 
-    expect(page).to have_content("Age: #{@dog_1.age}")
-    expect(page).to have_content("Age: #{@dog_2.age}")
-  end
+      dog_3.update(plays_fetch: true)
 
-  it 'can see all dog breeds' do
-    visit '/dogs'
+      visit '/dogs'
 
-    expect(page).to have_content("Breed: #{@dog_1.breed}")
-    expect(page).to have_content("Breed: #{@dog_2.breed}")
-  end
-
-  it 'can see all dog created at timestamps' do
-    visit '/dogs'
-
-    expect(page).to have_content("Created at: #{@dog_1.created_at}")
-    expect(page).to have_content("Created at: #{@dog_2.created_at}")
-  end
-
-  it 'can see all dog updated at timestamps' do
-    visit '/dogs'
-
-    expect(page).to have_content("Updated at: #{@dog_1.updated_at}")
-    expect(page).to have_content("Updated at: #{@dog_2.updated_at}")
-  end
-
-  it 'links to dog park index' do
-    visit '/dogs'
-
-    click_on 'Dog Parks Index'
-
-    expect(current_path).to eq('/dog_parks')
-  end
-
-  it 'links to dogs index' do
-    visit '/dogs'
-
-    click_on 'Dogs Index'
-
-    expect(current_path).to eq('/dogs')
+      expect(page).to have_content(dog_3.name)
+      expect(page).to have_content("Age: #{dog_3.age}")
+      expect(page).to have_content("Breed: #{dog_3.breed}")
+    end
   end
 end
