e373ab3ba38861f55b41f36d06f95a3027d3c8c4
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-10 18:15:35 -0600

    Add get and patch route for dog park update

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/dog_parks/new', to: 'dog_parks#new'
   post '/dog_parks', to: 'dog_parks#create'
   get '/dog_parks/:id', to: 'dog_parks#show'
+  get '/dog_parks/:id/edit', to: 'dog_parks#edit'
+  patch 'dog_parks/:id', to: 'dog_parks#update'
 
   get '/dog_parks/:dog_park_id/dogs', to: 'dog_park_dogs#index'
   get '/dog_parks/:dog_park_id/dogs/new', to: 'dog_park_dogs#new'
