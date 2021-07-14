2dc85a1c242f5fb0e5ce15317f3cef840ed201d2
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-14 12:40:01 -0600

    Refactor to remove unecessary variable creation

--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -2,23 +2,4 @@ require 'rails_helper'
 
 RSpec.describe Dog do
   it {should belong_to :dog_park}
-
-  # describe 'instance methods' do
-  #   before(:each) do
-  #     @park_1 = DogPark.create!(name: "Woofs and Wags",
-  #                             capacity: 25,
-  #                             off_leash: false,
-  #                             food: true)
-  #     @dog_1 = Dog.create!(name: 'Alfie',
-  #                             plays_fetch: true,
-  #                             age: 1.5,
-  #                             breed: 'Australian Shepherd',
-  #                             dog_park_id: @park_1.id)
-  #     @dog_2 = Dog.create!(name: 'Hazel',
-  #                             plays_fetch: true,
-  #                             age: 2,
-  #                             breed: 'Nova Scotia Duck Tolling Retriever',
-  #                             dog_park_id: @park_1.id)
-  #   end
-  # end
 end
