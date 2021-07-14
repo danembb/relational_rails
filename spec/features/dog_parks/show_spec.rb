b06bcbc55159f5989b6ad313ccf4ef7b14039962
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-14 17:02:46 -0600

    Update formatting with describe blocks

--- spec/features/dog_parks/show_spec.rb
+++ spec/features/dog_parks/show_spec.rb
@@ -27,90 +27,94 @@ RSpec.describe 'the dog park show page' do
                             dog_park_id: @park_2.id)
   end
 
-  it 'displays the dog park name' do
-    visit "/dog_parks/#{@park_1.id}"
+  describe 'displays dog park attributes' do
+    it 'shows the dog park name' do
+      visit "/dog_parks/#{@park_1.id}"
 
-    expect(page).to have_content(@park_1.name)
-    expect(page).to_not have_content(@park_2.name)
+      expect(page).to have_content(@park_1.name)
+      expect(page).to_not have_content(@park_2.name)
+    end
+
+    it 'shows the dog park capacity' do
+      visit "/dog_parks/#{@park_1.id}"
+
+      expect(page).to have_content("Capacity: #{@park_1.capacity}")
+      expect(page).to_not have_content("Capacity: #{@park_2.capacity}")
+    end
+
+    it 'shows if the dog park is off leash' do
+      visit "/dog_parks/#{@park_1.id}"
+
+      expect(page).to have_content("Off Leash Park: #{@park_1.off_leash}")
+      expect(page).to_not have_content("Off Leash Park: #{@park_2.off_leash}")
+    end
+
+    it 'shows if the dog park provides food' do
+      visit "/dog_parks/#{@park_1.id}"
+
+      expect(page).to have_content("Food for sale: #{@park_1.food}")
+      expect(page).to_not have_content("Food for sale: #{@park_2.food}")
+    end
+
+    it 'shows dog park created at' do
+      visit "/dog_parks/#{@park_1.id}"
+
+      expect(page).to have_content("Created at: #{@park_1.created_at}")
+    end
+
+    it 'shows dog park updated at' do
+      visit "/dog_parks/#{@park_1.id}"
+
+      expect(page).to have_content("Created at: #{@park_1.updated_at}")
+    end
+
+    it 'shows count dogs in the dog park' do
+      visit "/dog_parks/#{@park_1.id}"
+
+      expect(page).to have_content("Dog count: #{@park_1.dog_count}")
+    end
   end
 
-  it 'displays the dog park capacity' do
-    visit "/dog_parks/#{@park_1.id}"
+  describe 'hyperlinks' do
+    it 'links to dog park index' do
+      visit "/dog_parks/#{@park_1.id}"
 
-    expect(page).to have_content("Capacity: #{@park_1.capacity}")
-    expect(page).to_not have_content("Capacity: #{@park_2.capacity}")
-  end
+      click_on 'Dog Parks Index'
 
-  it 'displays if the dog park is off leash' do
-    visit "/dog_parks/#{@park_1.id}"
+      expect(current_path).to eq('/dog_parks')
+    end
 
-    expect(page).to have_content("Off Leash Park: #{@park_1.off_leash}")
-    expect(page).to_not have_content("Off Leash Park: #{@park_2.off_leash}")
-  end
+    it 'links to dogs index' do
+      visit "/dog_parks/#{@park_1.id}"
 
-  it 'displays if the dog park provides food' do
-    visit "/dog_parks/#{@park_1.id}"
+      click_on 'Dogs Index'
 
-    expect(page).to have_content("Food for sale: #{@park_1.food}")
-    expect(page).to_not have_content("Food for sale: #{@park_2.food}")
-  end
+      expect(current_path).to eq('/dogs')
+    end
 
-  it 'displays dog park created at' do
-    visit "/dog_parks/#{@park_1.id}"
+    it 'links to dog parks dogs page' do
+      visit "/dog_parks/#{@park_1.id}"
 
-    expect(page).to have_content("Created at: #{@park_1.created_at}")
-  end
+      click_on "Dogs in #{@park_1.name}"
 
-  it 'displays dog park updated at' do
-    visit "/dog_parks/#{@park_1.id}"
+      expect(current_path).to eq("/dog_parks/#{@park_1.id}/dogs")
+    end
 
-    expect(page).to have_content("Created at: #{@park_1.updated_at}")
-  end
+    it 'links to update dog park page' do
+      visit "/dog_parks/#{@park_1.id}"
 
-  it 'displays count dogs in the dog park' do
-    visit "/dog_parks/#{@park_1.id}"
+      click_on 'Update Dog Park'
 
-    expect(page).to have_content("Dog count: #{@park_1.dog_count}")
-  end
+      expect(current_path).to eq("/dog_parks/#{@park_1.id}/edit")
+    end
 
-  it 'links to dog park index' do
-    visit "/dog_parks/#{@park_1.id}"
+    it 'links to delete dog park' do
+      visit "/dog_parks/#{@park_1.id}"
 
-    click_on 'Dog Parks Index'
+      click_on 'Delete Dog Park'
 
-    expect(current_path).to eq('/dog_parks')
-  end
-
-  it 'links to dogs index' do
-    visit "/dog_parks/#{@park_1.id}"
-
-    click_on 'Dogs Index'
-
-    expect(current_path).to eq('/dogs')
-  end
-
-  it 'links to dog parks dogs page' do
-    visit "/dog_parks/#{@park_1.id}"
-
-    click_on "Dogs in #{@park_1.name}"
-
-    expect(current_path).to eq("/dog_parks/#{@park_1.id}/dogs")
-  end
-
-  it 'links to update dog park page' do
-    visit "/dog_parks/#{@park_1.id}"
-
-    click_on 'Update Dog Park'
-
-    expect(current_path).to eq("/dog_parks/#{@park_1.id}/edit")
-  end
-
-  it 'links to delete dog park' do
-    visit "/dog_parks/#{@park_1.id}"
-
-    click_on 'Delete Dog Park'
-
-    expect(current_path).to eq('/dog_parks')
-    expect(page).to_not have_content(@park_1.name)
+      expect(current_path).to eq('/dog_parks')
+      expect(page).to_not have_content(@park_1.name)
+    end
   end
 end
